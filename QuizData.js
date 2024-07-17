30cb7f6b6b31a3e331d7977da021c9302911a4eb
Soumyakanta Parida <120440650+Soumya234567890@users.noreply.github.com>
2024-07-17 20:48:10 +0530

    Add files via upload

---
+++ QuizData.js
@@ -0,0 +1,24 @@
+export const QuizData =[
+    {
+        question:"What is the purpose of the alt attribute in an HTML img tag?",
+        options:["To specify the image file name","To provide a tooltip for the image","To display a text description of the image for accessibility purposes","To set the image width and height"],
+        answer:3
+    },
+    {
+        question:" What is the difference between null and undefined in JavaScript?",
+        options:["null is an empty string, while undefined is a variable that has not been declared ","null is a variable that has not been declared, while undefined is an empty string","null represents the intentional absence of any object value, while undefined represents an uninitialized variable","null and undefined are interchangeable terms"],
+        answer:3
+    },
+    {
+        question:"What is the purpose of the key prop in a React component?",
+        options:["To specify the component's CSS class","To define the component's event handler","To help React keep track of the component's identity in a list of items","To pass data from a parent component to a child component"],
+        answer:3
+
+    },
+    {
+        question:" What is the primary purpose of HTTPS (Hypertext Transfer Protocol Secure)?",
+        options:["To compress web page data for faster loading ", "To cache web page resources for faster loading" ,"To encrypt data transmitted between the client and server to prevent eavesdropping" ,"To authenticate users before granting access to a website"],
+        answer:3
+    }
+
+];
